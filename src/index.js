dc6e9c5c3327edca34e66eafb1e5be67fd815abd
Pablo Dinella <pablo.dinella@rivendel.com.br>
2018-03-21 17:21:38 -0300

    MVP

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const axios = require('axios');
+
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  try {
+    const response = await axios.get('https://medium.com/elementarybr/latest?format=json')
+    console.log(response.status);
+    if (!response.data === 200) throw new Error('medium API did not return status 200')
+    const json = JSON.parse(response.data.replace('])}while(1);</x>', ''))
+    if (!json.success) throw new Error('medium JSON\'s success flag is not true')
+    res.end(JSON.stringify(json));
+  } catch (e) {
+    res.end(JSON.stringify({error: String(e)}))
+  }
+});
+
+app.listen(3000);
